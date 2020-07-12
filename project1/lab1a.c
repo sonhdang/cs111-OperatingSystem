1d30b3a81a534ce1d6497f8d0d6c08951d92b01d
Son Dang <sonhdang@ucla.edu>
2020-07-12 23:29:28 +0700

    Finished problem 1

--- project1/lab1a.c
+++ project1/lab1a.c
@@ -6,11 +6,14 @@
 
 int main(int argc, char *argv[])
 {
-
+    // Allocating memory for temios structs
     struct termios *original = malloc(sizeof(*original));
     struct termios *modified = malloc(sizeof(*modified));
+    // Setting buffer size to 5 to account for buffering
     char buf[5];
-    int counter = 0;
+    // Constant for Carriage Return and Line Feed <cr><lf>
+    const char NEWLINE[2] = {'\r','\n'};
+    const char EOT = 0x04;
     
     // printf("Size of original: %lu", sizeof(struct *termios));
     int result = tcgetattr(STDIN_FILENO, original);
@@ -42,18 +45,30 @@ int main(int argc, char *argv[])
 
     printf("modified c_iflag: %lu, c_oflag: %lu, c_lflag: %lu\n",
     modified->c_iflag, modified->c_oflag, modified->c_lflag);
+    
 
     tcflush(STDIN_FILENO, TCIFLUSH);
     tcsetattr(STDIN_FILENO, TCSANOW, modified);
 
-    while(counter < 20)
+    while(1)
     {
         read(STDIN_FILENO, buf, 5);
-        write(STDOUT_FILENO, buf, 5);
-        counter++;
-    }
+        //printf("%d", buf[0]);
+        if(buf[0] == '\r')
+            write(STDOUT_FILENO, NEWLINE, 2);
+        else if (buf[0] == EOT)
+        {
+            tcsetattr(STDOUT_FILENO, TCSANOW, original);
+            exit(0);
+        }
 
-    tcsetattr(STDOUT_FILENO, TCSANOW, original);
+            
+
+        write(STDOUT_FILENO, buf, 5);
+    }
+    tcflush(STDIN_FILENO, TCIOFLUSH);
+    
+    
     return 0;
 
 }
