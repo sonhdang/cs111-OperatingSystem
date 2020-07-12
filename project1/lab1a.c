52f50c133377cb42a720f84751966109790e40f1
Son Dang <sonhdang@ucla.edu>
2020-07-13 00:22:54 +0700

    Added support for option --shell=program with getopt_long

--- project1/lab1a.c
+++ project1/lab1a.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <errno.h>
+#include <getopt.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,37 +15,38 @@ int main(int argc, char *argv[])
     // Constant for Carriage Return and Line Feed <cr><lf>
     const char NEWLINE[2] = {'\r','\n'};
     const char EOT = 0x04;
-    
-    // printf("Size of original: %lu", sizeof(struct *termios));
-    int result = tcgetattr(STDIN_FILENO, original);
-    
+    int c;
+    while(1)
+    {
+        static struct option long_options[] = 
+        {
+            {"shell", required_argument, 0, 's'},
+            {0, 0, 0, 0}
+        };
 
-    if (result == 0)
-    {
-        printf("Succeeded\n");
+        int option_index = 0;
+        c = getopt_long_only(argc, argv, "", long_options, &option_index);
+
+        /* Detect the end of the option */
+        if (c == -1)
+            break;
+        
+        switch (c)
+        {
+            case 's':
+                printf("hello %s\n", optarg);
+                break;
+            default:
+                abort ();
+        }
     }
-    else
-    {
-        printf("Failed\n");
-    }
+
+    tcgetattr(STDIN_FILENO, original);
 
     *modified = *original;
-
-    printf("original c_iflag: %lu, c_oflag: %lu, c_lflag: %lu\n",
-    original->c_iflag, original->c_oflag, original->c_lflag);
-
-    printf("modified c_iflag: %lu, c_oflag: %lu, c_lflag: %lu\n",
-    modified->c_iflag, modified->c_oflag, modified->c_lflag);
-    
     modified->c_iflag = ISTRIP;
-    modified->c_oflag = 0;  // Map NL to CR-NL on output
-    modified->c_lflag = 0;    //set ICANON
-
-    printf("original c_iflag: %lu, c_oflag: %lu, c_lflag: %lu\n",
-    original->c_iflag, original->c_oflag, original->c_lflag);
-
-    printf("modified c_iflag: %lu, c_oflag: %lu, c_lflag: %lu\n",
-    modified->c_iflag, modified->c_oflag, modified->c_lflag);
+    modified->c_oflag = 0;
+    modified->c_lflag = 0;
     
 
     tcflush(STDIN_FILENO, TCIFLUSH);
@@ -53,19 +55,16 @@ int main(int argc, char *argv[])
     while(1)
     {
         read(STDIN_FILENO, buf, 5);
-        //printf("%d", buf[0]);
-        if(buf[0] == '\r')
+        if(buf[0] == '\r')  // Newline character
             write(STDOUT_FILENO, NEWLINE, 2);
-        else if (buf[0] == EOT)
+        else if (buf[0] == EOT) // Detecting EOT/EOF or ctrl-D
         {
             tcsetattr(STDOUT_FILENO, TCSANOW, original);
             exit(0);
         }
-
-            
-
         write(STDOUT_FILENO, buf, 5);
     }
+
     tcflush(STDIN_FILENO, TCIOFLUSH);
     
     
